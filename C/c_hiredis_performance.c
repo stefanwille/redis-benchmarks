be61a1efaaf4d1569deb338a317a265e9e1223fd
lodenos <nikolaiilodenos@gmail.com>
2018-12-15 19:43:41 +0000

    more accurate time counter with more explicit display in C

--- C/c_hiredis_performance.c
+++ C/c_hiredis_performance.c
@@ -1,33 +1,39 @@
-#include <stdio.h>
 #include <hiredis/hiredis.h>
+#include <stdio.h>
+#include <time.h>
 
-const int N = 1000000;
+#define NANO_IN_SEC 1000000000
+#define NBR_CALL 1000000
 
-int main () {
+int time_calculate(struct timespec begin, struct timespec end, int nbr) {
+  long long delta = (end.tv_sec * NANO_IN_SEC + end.tv_nsec) -
+                    (begin.tv_sec * NANO_IN_SEC + begin.tv_nsec);
+  return (int)(nbr / (delta / (double)NANO_IN_SEC));
+}
+
+int main(void) {
+  struct timespec begin = {0};
+  struct timespec end = {0};
   printf("Connecting...\n");
   redisContext *redis = redisConnect("localhost", 6379);
-  if(redis->err) {
+  if (redis->err) {
     puts(redis->errstr);
     char *p = redis->errstr;
-    while(*p) {
-      printf("%x ", (int) (*p++));
-    }
+    while (*p)
+      printf("%x ", (int)(*p++));
     printf("\n");
   } else {
     puts("Connected!");
-    // void *redisCommand(redisContext *c, const char *format, ...);
-
-    for(int i = 0; i < N; i++) {
-      redisAppendCommand(redis,"SET foo bar");
-    }
-
-    for(int i = 0; i < N; i++) {
+    clock_gettime(CLOCK_MONOTONIC, &begin);
+    for (int i = 0; i < NBR_CALL; i++)
+      redisAppendCommand(redis, "SET foo bar");
+    for (int i = 0; i < NBR_CALL; i++) {
       redisReply *reply;
-      redisGetReply(redis, (void**) &reply); // reply for SET
+      redisGetReply(redis, (void **)&reply);
       freeReplyObject(reply);
     }
-
+    clock_gettime(CLOCK_MONOTONIC, &end);
     puts("Done");
   }
+  printf("> %d c/sec\n", time_calculate(begin, end, NBR_CALL));
 }
-
